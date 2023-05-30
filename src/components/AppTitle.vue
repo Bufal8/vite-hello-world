35d46affb38059b9ccd9c7acd8b9e0959be54ca3
ManuelF <manuel.floriani1999@gmail.com>
2023-05-30 16:11:25 +0200

    Creazione nuovo componente Vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<!-- Descrizione:
+Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell’installazione come visti a lezione.
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita “La mia prima app con Vite!”
+Bonus:
+Creare un secondo componente AppHero, che visualizza un jumbotron. -->
+
+<script>
+import { ref } from 'vue'
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>La mia prima app con Vite!🔩</h1>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
